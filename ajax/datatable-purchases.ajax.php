e762a57a4c6b613f4dfa8a78d721dd68a590187d
ch15 <ch15solito@gmail.com>
2024-01-12 12:51:49 +0800

    Add 'idDescription' to access products[description] from Database

--- ajax/datatable-purchases.ajax.php
+++ ajax/datatable-purchases.ajax.php
@@ -3,24 +3,29 @@
 require_once "../controllers/purchases.controller.php";
 require_once "../models/purchases.model.php";
 
+require_once "../controllers/products.controller.php";
+require_once "../models/products.model.php";
+
 require_once "../controllers/suppliers.controller.php";
 require_once "../models/suppliers.model.php";
 
-class purchasesTable{
+class purchasesTable
+{
 
 	/*=============================================
-               SHOW PRODUCTS TABLE
+               SHOW purchase TABLE
   	=============================================*/
-	
-	public function showPurchaseTable(){
+
+	public function showPurchaseTable()
+	{
 
 		$item = null;
 		$value = null;
 		$order = "id";
 
-		$products = controllerPurchases::ctrShowPurchases($item, $value, $order);
+		$purchase = controllerPurchases::ctrShowPurchases($item, $value, $order);
 
-		if(count($products) == 0){
+		if (count($purchase) == 0) {
 
 			$jsonData = '{"data":[]}';
 
@@ -32,51 +37,56 @@ class purchasesTable{
 		$jsonData = '{
 			"data":[';
 
-				for($i=0; $i < count($products); $i++){
+		for ($i = 0; $i < count($purchase); $i++) {
 
-					/*=============================================
+			/*=============================================
 			                		We bring the image
 					=============================================*/
-					
-					$image = "<img src='".$products[$i]["image"]."' width='40px'>";
 
-					/*=============================================
+			$image = "<img src='" . $purchase[$i]["image"] . "' width='40px'>";
+
+			/*=============================================
 				                	We bring the category
 					=============================================*/
-					
-					$item = 'id';
-				  	$value = $products[$i]['idCategory'];
 
-				  	$categories = ControllerSupplier::ctrCreateSupplier($item, $value);
-				
-		  			/*=============================================
+			$item = 'id';
+			$value = $purchase[$i]['idSupplier'];
+
+			$suppliers = ControllerSupplier::ctrShowSupplier($item, $value);
+
+			$item = "id";
+			$value = $purchase[$i]['idDescription'];
+			$order = "id";
+
+			$products = controllerProducts::ctrShowProducts($item, $value, $order);
+
+			/*=============================================
 		 	 	                	ACTION BUTTONS
-		  			=============================================*/ 
-					
-		  			if (isset($_GET["hiddenProfile"]) && $_GET["hiddenProfile"] == "special") {
+		  			=============================================*/
 
-		  				$buttons =  "<div class='btn-group'><button class='btn btn-primary btnEditProduct' idPurchase='".$products[$i]["id"]."' data-bs-toggle='modal' data-bs-target='#modalEditProduct'><i class='fa fa-pencil'></i></button></div>";
-		  			}
-		  			else{
-		  				$buttons =  "<div class='btn-group'><button class='btn btn-primary btnEditProduct' idPurchase='".$products[$i]["id"]."' data-bs-toggle='modal' data-bs-target='#modalEditProduct'><i class='fa fa-pencil'></i></button><button class='btn btn-danger btnDeleteProduct' idPurchase='".$products[$i]["id"]."' code='".$products[$i]["code"]."' image='".$products[$i]["image"]."'><i class='fa fa-trash'></i></button></div>";
-		  			}
+			if (isset($_GET["hiddenProfile"]) && $_GET["hiddenProfile"] == "special") {
 
-					$jsonData .='[
-						"'.($i+1).'",
-						"'.$image.'",
-						"'.$products[$i]["code"].'",
-						"'.$products[$i]["description"].'",
-						"'.$categories["Supplier"].'",
-						"'.$products[$i]["stock"].'",
-						"$ '.$products[$i]["buyingPrice"].'",
-						"$ '.$products[$i]["sellingPrice"].'",
-						"'.$products[$i]["date"].'",
-						"'.$buttons.'"
+				$buttons =  "<div class='btn-group'><button class='btn btn-primary btnEditProduct' idPurchase='" . $purchase[$i]["id"] . "' data-bs-toggle='modal' data-bs-target='#modalEditProduct'><i class='fa fa-pencil'></i></button></div>";
+			} else {
+				$buttons =  "<div class='btn-group'><button class='btn btn-primary btnEditProduct' idPurchase='" . $purchase[$i]["id"] . "' data-bs-toggle='modal' data-bs-target='#modalEditProduct'><i class='fa fa-pencil'></i></button><button class='btn btn-danger btnDeleteProduct' idPurchase='" . $purchase[$i]["id"] . "' code='" . $purchase[$i]["code"] . "' image='" . $purchase[$i]["image"] . "'><i class='fa fa-trash'></i></button></div>";
+			}
+
+			$jsonData .= '[
+						"' . ($i + 1) . '",
+						"' . $image . '",
+						"' . $purchase[$i]["code"] . '",
+						"' . $products["description"] . '",
+						"' . $suppliers["Supplier"] . '",
+						"' . $purchase[$i]["stock"] . '",
+						"$ ' . $purchase[$i]["buyingPrice"] . '",
+						"$ ' . $purchase[$i]["sellingPrice"] . '",
+						"' . $purchase[$i]["date"] . '",
+						"' . $buttons . '"
 					],';
-				}
+		}
 
-				$jsonData = substr($jsonData, 0, -1);
-				$jsonData .= '] 
+		$jsonData = substr($jsonData, 0, -1);
+		$jsonData .= '] 
 
 			}';
 
@@ -85,8 +95,8 @@ class purchasesTable{
 }
 
 /*=============================================
-          ACTIVATE PRODUCTS TABLE
-=============================================*/ 
+          ACTIVATE purchase TABLE
+=============================================*/
 
-$activateProducts = new purchasesTable();
-$activateProducts->showPurchaseTable();
+$activatepurchase = new purchasesTable();
+$activatepurchase->showPurchaseTable();
